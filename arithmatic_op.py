cd7e00a56a00f49599db567325ca7968c1f5ff42
shubhamchuadhari249814 <shubhamchaudhari2498@gmail.com>
2023-04-24 20:50:47 +0530

    shubham assignment multiplication

--- arithmatic_op.py
+++ arithmatic_op.py
@@ -3,3 +3,10 @@ no1=20
 no2=10
 ans = no1+no2
 print(f"Addition of {no1} and {no2} is {ans}")
+
+
+# code for Multiplication :
+no1=20
+no2=10
+ans = no1 * no2
+print(f"Multiplication of {no1} and {no2} is {ans}")
