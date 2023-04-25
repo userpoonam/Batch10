1d54f4269af3a7ab4f28b4e2b577956e982892f3
ganeshmali5065 <malig5065@gmail.com>
2023-04-25 20:37:34 +0530

    ganu substraction

--- arithmatic_op.py
+++ arithmatic_op.py
@@ -3,3 +3,11 @@ no1=20
 no2=10
 ans = no1+no2
 print(f"Addition of {no1} and {no2} is {ans}")
+
+
+# code for subtraction
+no1=20
+no2=10
+ans = no1-no2
+print(f"subtraction of {no1} and {no2} is {ans}")
+
