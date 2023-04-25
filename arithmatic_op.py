612d20bbf82dad332484321a30d1ec1b932ef809
shradhabhoiteo <shradhabhoite09@gmail.com>
2023-04-25 20:16:05 +0530

    Division Code

--- arithmatic_op.py
+++ arithmatic_op.py
@@ -3,3 +3,11 @@ no1=20
 no2=10
 ans = no1+no2
 print(f"Addition of {no1} and {no2} is {ans}")
+
+
+# code for division
+no1=20
+no2=10
+ans = no1/no2
+print(f"division of {no1} and {no2} is {ans}")
+
