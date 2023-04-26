fd617c584e1a2fa010729edd4baa8f0af22286f0
shubhamchuadhari249814 <shubhamchaudhari2498@gmail.com>
2023-04-26 17:03:40 +0530

    shubh_power_code

--- arithmatic_op.py
+++ arithmatic_op.py
@@ -12,7 +12,6 @@ ans = no1-no2
 print(f"subtraction of {no1} and {no2} is {ans}")
 
 
-
 # code for division
 no1=20
 no2=10
@@ -26,3 +25,12 @@ no2=10
 ans = no1 * no2
 print(f"Multi[lication of {no1} and {no2} is {ans}")
 
+
+# Code for finding power :
+def calc_power(N, p):
+    if p == 0:
+        return 1
+    return N * calc_power(N, p - 1)
+
+
+print(calc_power(2, 2))
